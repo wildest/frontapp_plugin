2f286c9972e5b3ec3334c4f7f9ce79b45cc30433
wildest <tom.wildes@gmail.com>
2015-06-04 14:18:51 -0400

    starting things up

---
+++ test-plugin.js
@@ -0,0 +1,91 @@
+var conversation;
+
+function unassign() {
+  Front.unassign(conversation);
+}
+
+function toggleArchive() {
+  Front.toggleArchive(conversation);
+}
+
+function toggleTrashed() {
+  Front.toggleTrashed(conversation);
+}
+
+function reply() {
+  Front.reply({
+    body: 'Template reply',
+    subject: 'Template subject',
+  }, false, conversation);
+}
+
+function alertDialog() {
+  Front.dialog('alert', {
+    title: 'I\'m an alert dialog',
+    message: 'You are now alerted',
+  }, function () {
+    console.log('Alert closed');
+  });
+}
+
+function confirmDialog() {
+  Front.dialog('confirm', {
+    title: 'I\'m a confirm dialog',
+    message: 'Do you confirm',
+    okTitle: 'OK Button',
+    cancelTitle: 'Cancel Button'
+  }, function (confirmed) {
+    if (confirmed)
+      console.log('User confirmed');
+    else
+      console.log('User cancelled');
+  });
+}
+
+function promptDialog() {
+  Front.dialog('prompt', {
+    title: 'I\'m a prompt dialog',
+    message: 'Please enter something'
+  }, function (data) {
+    if (data)
+      console.log('User input :', data);
+    else
+      console.log('User cancelled');
+  });
+}
+
+function fetchTeammates() {
+  Front.fetchAllowedTeammates(function (teammates) {
+    if (!teammates)
+      return;
+
+    console.log(teammates);
+  });
+}
+
+function fetchInboxes() {
+  Front.fetchInboxes(function (inboxes) {
+    if (!inboxes)
+      return;
+
+    console.log(inboxes);
+  });
+}
+
+function fetchTags() {
+  Front.fetchAllowedTags(function (tags) {
+    if (!tags)
+      return;
+
+    console.log(tags);
+  });
+}
+
+
+Front.on('conversation', function (data) {
+  console.log('Conversation', data.conversation);
+  console.log('Contact', data.contact);
+  console.log('Message', data.message);
+  console.log('OtherMessages', data.otherMessages);
+  conversation = data.conversation;
+});
